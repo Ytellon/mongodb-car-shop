999b84381ae74714712021b460266b6f0b9ba287
ytellon <ytellonmacedo@gmail.com>
2022-10-24 18:39:57 -0300

    fix: correcao no erro no middleware

--- src/middleware/errorsMid.ts
+++ src/middleware/errorsMid.ts
@@ -9,14 +9,14 @@ const errorHandler: ErrorRequestHandler = (
   _next,
 ) => {
   if (err instanceof ZodError) {
-    return res.status(400).json({ message: err.issues });
+    return res.status(400).json({ error: err.issues });
   }
 
   const messageAsErrorType = err.message as ErrorTypes;
   const mappedError = errorCatalog[messageAsErrorType];
   if (mappedError) {
-    const { httpStatus, message } = mappedError;
-    return res.status(httpStatus).json({ message });
+    const { httpStatus, error } = mappedError;
+    return res.status(httpStatus).json({ error });
   }
 
   console.error(err);
