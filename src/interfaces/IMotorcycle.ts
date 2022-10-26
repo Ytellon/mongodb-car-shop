ab74a73b479732a45da37d4a1ceba94152ac43e5
ytellon <ytellonmacedo@gmail.com>
2022-10-25 22:09:10 -0300

    feat: feito a interface IMotorcycle a partir da Interface IVehicle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+import { VehicleSchema } from './IVehicle'; 
+
+export const MotorSchema = VehicleSchema.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().int().lte(2500),
+});
+
+export type IMotorcycle = z.infer<typeof MotorSchema>;
